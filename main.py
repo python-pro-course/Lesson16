bc6cd123a101136fadd09119b29808ad3e2aa528
Алина Шириханова <alina.meller.98@mail.ru>
2022-11-12 13:02:40 +0300

    created class BankAccount

--- main.py
+++ main.py
@@ -2,14 +2,44 @@ from tkinter import *
 import os
 from PIL import Image, ImageTk
 
+
+class BankAccount:
+    def __init__(self, login, password, balance=0):
+        self.login = login
+        self.password = password
+        self.balance = balance
+        self.create_user()
+
+    def create_user(self):
+        if self.login == '' or self.password == '':
+            notif.config(text='Необходимо заполнить все поля', fg='red')
+
+
+
+
+
+
 # Создание главного окна
 main = Tk()  # Tk() - конструктор, инициализирующий окно
 main.title('Банковая система')  # устанавливаем заголовок окна
 main.geometry('300x300')  # устанавливаем размер окна
 
 
+def create_account():
+    login = s_up_login.get()
+    password = s_up_pass.get()
+    BankAccount(login, password)
+
+
+
 # Содздание окна регистрации
 def sign_up():
+    global s_up_login
+    global s_up_pass
+    global notif
+    s_up_login = StringVar()
+    s_up_pass = StringVar()
+
     sign_up_screen = Toplevel(main)
     sign_up_screen.title('Sign Up')
     main.geometry('200x200')
@@ -18,8 +48,16 @@ def sign_up():
     Label(sign_up_screen, text='Введите логин и пароль').grid(row=0, sticky=N, pady=10)  # row - строка
     Label(sign_up_screen, text='Login').grid(row=1, sticky=W)
     Label(sign_up_screen, text='Password').grid(row=2, sticky=W)
+    notif = Label(sign_up_screen, font=('Calibri', 12))
+    notif.grid(row=4, sticky=N, pady=10)
 
+    # Entries
+    Entry(sign_up_screen, textvariable=s_up_login).grid(row=1, column=1)
+    Entry(sign_up_screen, textvariable=s_up_pass).grid(row=2, column=1)
 
+    # Buttons
+    Button(sign_up_screen, text='Sign In', font=('Calibri', 12), width=15, command=create_account).grid(row=3, sticky=N,
+                                                                                                       pady=10)
 
 
 # Создание картинки
